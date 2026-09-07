6b4a8f86bb494490b69506dd3cefeb499f3da0ec
0xsaif <muhamed.e.saif@gmail.com>
2026-09-08 01:40:21 +0300

    wip: implement memory and direct access cases

--- part-1/handmade_disassembler.c
+++ part-1/handmade_disassembler.c
@@ -1,53 +1,27 @@
-/*
- * we're reading a 2-byte binary file
- * our goal is to decode/disassemble the bytes into the mov instruction
- * 2-byte 8 bit structure:
- *  - first byte:
- *      - [:6]  opcode
- *      - [6:7] D = destination flag (to register or from register)
- *          - 1 -> REG is the destination register
- *          - 0 -> REG is the source register
- *      - [7]   W = word size flag (word/byte operation)
- *          - 1 -> (word) word size is 16 bits
- *          - 0 -> (byte) word size is 8 bits
- *  - second byte: operand
- *      - [:2] mod (identifies whether one of the operands is in memory or
- * whether both operands are registers)
- *          - 11 -> register operation
- *      - [2:5] reg (register)
- *      - [5:7] r/m (register/memory)
- *
- * we're just decoding one single format: move from register to register = MOV
- * REG, REG
- * furthur, we're just decoding one single CASE: move from bx to cx =
- * MOV cx, bx looking at the Table 4.9 in the 8086 manual:
- *  - cx = 001
- *  - bx = 011
- *  - Both are 16-bit size
- * = 100010 11 - 11 001 011
- * =
- */
-
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 
-int main(void) {
-  FILE *fp = fopen("listing_0037_single_register_mov", "rb");
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    return 1;
+  }
+
+  FILE *fp = fopen(argv[1], "rb");
   if (fp == NULL) {
     perror("fopen");
     return 1;
   }
 
   uint8_t bytes[2];
-  // read 2 elements, each of 1 byte long
+  // read 2 bytes
   size_t count = fread(bytes, 1, 2, fp);
   if (count != 2) {
     fprintf(stderr, "expected 2 bytes, got %zu\n", count);
     fclose(fp);
     return 1;
   }
-  fclose(fp);
 
   uint8_t first_byte = bytes[0];
   uint8_t opcode = first_byte >> 2;
@@ -60,37 +34,80 @@ int main(void) {
   }
 
   uint8_t second_byte = bytes[1];
-  uint8_t mod = (second_byte & 0b11000000) >> 6;
-  uint8_t reg = (second_byte & 0b00111000) >> 3;
-  uint8_t rm = (second_byte & 0b00000111);
+  uint8_t mod = (second_byte & 0xc0) >> 6;
+  uint8_t reg = (second_byte & 0x38) >> 3;
+  uint8_t rm = (second_byte & 0x7);
 
-  if (mod != 0b11) {
-    printf("register mode only supported");
-    return 1;
-  }
-
-  /*
-   * the goal is to know which register operands are
-   */
-
-  const char *registers_8bit_map[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
+  const char *registers_8bit_map[] = {"al", "cl", "dl", "bl",
+                                      "ah", "ch", "dh", "bh"};
 
   const char *registers_16bit_map[] = {"ax", "cx", "dx", "bx",
-                                 "sp", "bp", "si", "di"};
+                                       "sp", "bp", "si", "di"};
+
+  const char *rm_address_expression[] = {
+      "[BX + SI]", "[BX + DI]", "[BP + SI]", "[BP + DI]",
+      "[SI]",      "[DI]",      "[BP]",      "[BX]",
+  };
 
   const char **registers;
   const char *width;
 
   if (W == 0) {
     registers = registers_8bit_map;
-    width = "8 bits";
+    width = "8";
   } else {
     registers = registers_16bit_map;
-    width = "16 bits";
+    width = "16";
   }
 
   const char *reg_name = registers[reg];
-  const char *rm_name = registers[rm];
+
+  const char *rm_address_exp;
+  int read_extra_bytes;
+  int is_direct_access = 0;
+  switch (mod) {
+  case 0b00:
+    if (rm == 0b110) {
+      is_direct_access = 1;
+    } else {
+      rm_address_exp = rm_address_expression[rm];
+      read_extra_bytes = 0;
+    }
+    break;
+  case 0b01:
+    rm_address_exp = rm_address_expression[rm];
+    read_extra_bytes = 1;
+    break;
+  case 0b10:
+    rm_address_exp = rm_address_expression[rm];
+    read_extra_bytes = 2;
+    break;
+  case 0b11:
+    rm_address_exp = registers[rm];
+    read_extra_bytes = 0;
+    break;
+  }
+
+  int total_num_of_bytes = 2 + read_extra_bytes;
+  uint8_t bytes2[total_num_of_bytes];
+  if (total_num_of_bytes > 2) {
+    int n = fread(bytes2, 1, total_num_of_bytes, fp);
+    if (n != 2) {
+      fprintf(stderr, "expected %i bytes, got %i\n", total_num_of_bytes, n);
+      fclose(fp);
+      return 1;
+    }
+    fclose(fp);
+
+    uint8_t third_byte = bytes2[2];
+    uint16_t the_full_extra_byte;
+    if (total_num_of_bytes == 3) {
+      the_full_extra_byte = third_byte;
+    } else if (total_num_of_bytes == 4) {
+      uint8_t fourth_byte = bytes2[3];
+      the_full_extra_byte = third_byte | (fourth_byte << 8);
+    }
+  }
 
   /*
    * After we know the register we should know which one is the src
@@ -100,9 +117,9 @@ int main(void) {
   const char *src, *dest;
   if (D == 0) {
     src = reg_name;
-    dest = rm_name;
+    dest = rm_address_exp;
   } else if (D == 1) {
-    src = rm_name;
+    src = rm_address_exp;
     dest = reg_name;
   }
 
